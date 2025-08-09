815972b4eceaa1b96bbec19a5fa8e1fb6bba195e
kevin <snowdenxedward@gmail.com>
2025-08-10 04:52:03 +0530

    chore(database): add initial database connection setup in db.js

---
+++ database/db.js
@@ -0,0 +1,20 @@
+import mysql2 from 'mysql2/promise'
+import dotenv from 'dotenv'
+dotenv.config()
+
+async function connectDB() {
+    try {
+        const connection = await mysql2.createConnection({
+            host: process.env.HOST,
+            user: process.env.USER,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
+        })
+        return connection
+    } catch (error) {
+        return error.message
+    }
+}
+
+const connection = await connectDB()
+export default connection
